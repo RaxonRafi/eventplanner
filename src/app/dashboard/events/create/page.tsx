b7a93f90719a207983d7744f371a3b2a3e857f31
Rafi <itzmeh320@gmail.com>
2025-10-01 15:58:57 +0600

    fixed

--- src/app/dashboard/events/create/page.tsx
+++ src/app/dashboard/events/create/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { EventList } from "@/components/Admin/EventList";
 import { CreateEventForm } from "@/components/forms/createEventForm";
-import { OrgEventList } from "@/components/Organizer/OrgEventList";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
-import { useUserInfoQuery } from "@/redux/features/User/user.api";
 
 const Page = () => {
-  const { data, isLoading } = useUserInfoQuery(undefined);
-  const role = data?.data?.role;
-
   return (
     <SidebarInset>
       <header className="flex h-16 shrink-0 items-center gap-2 px-4">
@@ -22,7 +16,7 @@ const Page = () => {
       </header>
 
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <CreateEventForm/>
+        <CreateEventForm />
       </div>
     </SidebarInset>
   );
