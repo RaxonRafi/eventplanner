2dd72fa921d306ea10526e32c4b1cf73ff48a1f6
Rafi <itzmeh320@gmail.com>
2025-10-01 16:06:16 +0600

    Hero fixed

--- src/components/forms/registerForm.tsx
+++ src/components/forms/registerForm.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -13,13 +11,13 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useRegisterMutation } from "@/redux/features/User/user.api";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Role } from "@prisma/client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 const registerSchema = z
   .object({
@@ -28,12 +26,13 @@ const registerSchema = z
       .min(3, {
         error: "Name is too short",
       })
-      .max(50,{ message: "Name is too long" }),
+      .max(50, { message: "Name is too long" }),
     email: z.email(),
-    role: z.enum([Role.USER, Role.ORGANIZER])
-    .refine((val) => val === Role.USER || val === Role.ORGANIZER, {
-      message: "Role must be user or organizer",
-    }),
+    role: z
+      .enum([Role.USER, Role.ORGANIZER])
+      .refine((val) => val === Role.USER || val === Role.ORGANIZER, {
+        message: "Role must be user or organizer",
+      }),
     password: z
       .string({ message: "Password is required" })
       .min(8, { message: "Password must be at least 8 characters long." })
@@ -51,7 +50,6 @@ export function RegisterForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
-
   const [register, { isLoading }] = useRegisterMutation();
   const router = useRouter();
 
@@ -64,7 +62,7 @@ export function RegisterForm({
       password: "",
       cpassword: "",
     },
-    mode: "onSubmit"
+    mode: "onSubmit",
   });
 
   const onSubmit = async (data: z.infer<typeof registerSchema>) => {
@@ -72,91 +70,86 @@ export function RegisterForm({
       name: data.name,
       email: data.email,
       role: data.role,
-      password: data.password
-    }
-    try{
+      password: data.password,
+    };
+    try {
       const result = await register(userInfo).unwrap();
       console.log(result);
-      toast.success("Registered Successfully!")
-      router.push("/login")
-    } catch(error){
+      toast.success("Registered Successfully!");
+      router.push("/login");
+    } catch (error) {
       console.log(error);
-      toast.error("Registration Failed!")
+      toast.error("Registration Failed!");
     }
   };
 
-
   return (
     <Form {...form}>
-    <form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-2xl font-bold">Welcome!</h1>
-          <p className="text-muted-foreground text-balance">
-            Register to your UrBoxMate account
-          </p>
-        </div>
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your name....." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your Email....." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="role"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormLabel>Role</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex"
-                >
-                  <FormItem className="flex items-center gap-3">
-                    <FormControl>
-                      <RadioGroupItem value={Role.USER} />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      User
-                    </FormLabel>
-                  </FormItem>
-                  <FormItem className="flex items-center gap-3">
-                    <FormControl>
-                      <RadioGroupItem value={Role.ORGANIZER} />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      Organizer
-                    </FormLabel>
-                  </FormItem>
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
+      <form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center text-center">
+            <h1 className="text-2xl font-bold">Welcome!</h1>
+            <p className="text-muted-foreground text-balance">
+              Register to your UrBoxMate account
+            </p>
+          </div>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your name....." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your Email....." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Role</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex"
+                  >
+                    <FormItem className="flex items-center gap-3">
+                      <FormControl>
+                        <RadioGroupItem value={Role.USER} />
+                      </FormControl>
+                      <FormLabel className="font-normal">User</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center gap-3">
+                      <FormControl>
+                        <RadioGroupItem value={Role.ORGANIZER} />
+                      </FormControl>
+                      <FormLabel className="font-normal">Organizer</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
             control={form.control}
             name="password"
             rules={{
@@ -173,7 +166,7 @@ export function RegisterForm({
               </FormItem>
             )}
           />
-        <FormField
+          <FormField
             control={form.control}
             name="cpassword"
             rules={{
@@ -190,17 +183,17 @@ export function RegisterForm({
               </FormItem>
             )}
           />
-        <Button type="submit" className="w-full">
-          Submit
-        </Button>
-        <div className="text-center text-sm">
-          Already have an account?{" "}
-          <Link href="/login" className="underline underline-offset-4">
-            login
-          </Link>
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+          <div className="text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/login" className="underline underline-offset-4">
+              login
+            </Link>
+          </div>
         </div>
-      </div>
-    </form>
-  </Form>
+      </form>
+    </Form>
   );
 }
